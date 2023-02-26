80a64b889e11b3cccc326ce9309fb35a3412fb5b
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-25 18:33:45 -0600

    create musical Markov Chain

--- videos/dictionaries_and_markov_chains/main.py
+++ videos/dictionaries_and_markov_chains/main.py
@@ -1,1 +1,14 @@
-pass
+import musicalbeeps
+import random
+
+random.seed(43)  # change me
+
+player = musicalbeeps.Player(volume=0.3, mute_output=False)
+
+markov_chain = {'A': ['A', 'G'], 'G': ['A']}  # maybe change me too?
+
+note = 'A'
+
+for _ in range(16):
+    player.play_note(note, 0.5)
+    note = random.choice(markov_chain[note])
